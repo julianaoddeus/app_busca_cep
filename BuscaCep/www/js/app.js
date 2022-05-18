6ae28bd2981dabaec0df4b46a09cf5fa6d48cd24
Juliana <64862169+julianaoddeus@users.noreply.github.com>
2022-05-18 20:03:29 -0300

    criado HTML

---
+++ BuscaCep/www/js/app.js
@@ -0,0 +1,10 @@
+let cep = document.querySelector('#cep')
+
+function BuscaCep(){
+        console.log("Essa é a função de pesquisa de cep")
+}
+
+let btnPesquisar = document.querySelector('#pesquisar');
+btnPesquisar.addEventListener('click', function(){
+
+})
